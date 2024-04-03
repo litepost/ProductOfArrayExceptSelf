49d18d5d3c0be444ce7eccfa5d964934e1263fc8
litepost <litepost@gmail.com>
2024-04-02 20:57:25 -0700

    ready to solve problem

--- ProductOfArrayExceptSelf/Program.cs
+++ ProductOfArrayExceptSelf/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://leetcode.com/problems/product-of-array-except-self/description/
+
+int[] nums = [1, 2, 3, 4];
+// int[] nums = [-1,1,0,-3,3];
+
+int[] answer = ProductExceptSelf(nums);
+
+
+static int[] ProductExceptSelf(int[] nums) {
+    int[] left = new int[nums.Length];
+    int[] right = new int[nums.Length];
+    Array.Fill(left, 1);
+    Array.Fill(right, 1);
+
+    return left;
+}
